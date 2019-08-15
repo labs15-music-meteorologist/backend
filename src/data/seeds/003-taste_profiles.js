2de8a8047761132ca6cb719377eb91c84dcf11e2
Sascha Majewsky <sascha.majewsky@pm.me>
2019-08-16 01:33:26 +0200

    Updated taste_profiles seed for database population
    Co-authored-by: Justin Chen <justiny2c@gmail.com>
    Co-authored-by: Ben Feole <bfeole@gmail.com>

--- src/data/seeds/003-taste_profiles.js
+++ src/data/seeds/003-taste_profiles.js
@@ -1,23 +1,23 @@
 exports.seed = function(knex) {
-  return knex('users')
+  return knex('taste_profiles')
     .del()
     .then(function() {
       return knex('taste_profiles').insert([
         {
           user_id: 1,
-          acousticness: '.0294',
-          danceability: '.88',
-          energy: '.87',
-          instrumentalness: '.81',
-          key: '11',
-          liveness: '.094',
-          loudness: '1.1',
-          mode: '1',
-          speechiness: '.02',
-          tempo: '120',
-          time_signature: '4',
-          popularity: '55',
-          valence: '.64',
+          acousticness: 0.0294,
+          danceability: 0.88,
+          energy: 0.87,
+          instrumentalness: 0.81,
+          key: 11,
+          liveness: 0.094,
+          loudness: 1.1,
+          mode: 1,
+          speechiness: 0.02,
+          tempo: 120,
+          time_signature: 4,
+          popularity: 55,
+          valence: 0.64,
         },
       ]);
     });
