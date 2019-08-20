e34185c094e15e58b2c182d4b3539b734e99bee5
Sascha Majewsky <sascha.majewsky@pm.me>
2019-08-20 03:45:24 +0200

    Refactored development environment for knex heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,18 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
-    useNullAsDefault: true, // needed for sqlite
+    client: 'pg',
     connection: {
-      filename: './data/splitthebill.db3',
+      database: process.env.DATABASE_DB,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
     },
+    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations',
+      tableName: 'knex_migrations',
     },
     seeds: {
       directory: './data/seeds',
-    },
-    // needed to use foreign keys
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
     },
   },
   production: {
