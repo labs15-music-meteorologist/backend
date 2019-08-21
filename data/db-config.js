8fa63e41b8b3a7023af3d4a6dd2ade78d8fefb38
Sascha Majewsky <sascha.majewsky@pm.me>
2019-08-21 18:07:39 +0200

    :recycle: Refactored validation check for user object request body not beeing present

--- data/db-config.js
+++ data/db-config.js
@@ -1,4 +1,5 @@
 const knex = require('knex');
+require('dotenv').config();
 
 const knexConfig = require('../knexfile.js');
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
+require('dotenv').config();
 
 const UsersRouter = require('./api/users/user-router.js');
 const server = express();
